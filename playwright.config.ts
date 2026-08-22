747eded5062451daa4e461401c19277de46b4db9
Denys Ishchuk <ischukdenni@gmail.com>
2026-08-22 15:57:48 +0000

    refactor: configure api project in playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
+      testIgnore: "**/api/**/*.spec.ts",
       use: {
         ...devices["Desktop Chrome"],
       },
@@ -37,6 +38,7 @@ export default defineConfig({
 
     {
       name: "firefox",
+      testIgnore: "**/api/**/*.spec.ts",
       use: {
         ...devices["Desktop Firefox"],
       },
@@ -44,8 +46,23 @@ export default defineConfig({
 
     {
       name: "webkit",
+      testIgnore: "**/api/**/*.spec.ts",
       use: {
         ...devices["Desktop Safari"],
+      },
+    },
+
+    {
+      name: "api",
+      testMatch: "**/api/**/*.spec.ts",
+      use: {
+        baseURL: "https://reqres.in",
+        extraHTTPHeaders: {
+          Accept: "application/json",
+          "x-api-key": reqresApiKey,
+          "User-Agent": "playwright-api-tests/1.0",
+          "X-Reqres-Env": "prod",
+        },
       },
     },
   ],
